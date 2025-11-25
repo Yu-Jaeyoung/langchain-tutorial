cf25ab884eb2e1ca835f9ea03640cc3bc6f9411b
Yu-Jaeyoung <jackyu0721@gmail.com>
2025-11-25 14:41:03 +0900

    runnable sequence example / using arrow function for prev-result

---
+++ runnable-sequence/index.ts
@@ -0,0 +1,39 @@
+import { ChatOpenAI } from "@langchain/openai";
+import { PromptTemplate } from "@langchain/core/prompts";
+import { RunnableSequence } from "@langchain/core/runnables";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+
+const openAIApiKey = process.env.OPENAI_API_KEY;
+const llm = new ChatOpenAI({
+  model: "gpt-4o-mini",
+  openAIApiKey,
+});
+
+const punctuationTemplate = `Given a sentence, add punctuation where needed. 
+    sentence: {sentence}
+    sentence with punctuation:  
+    `;
+const punctuationPrompt = PromptTemplate.fromTemplate(punctuationTemplate);
+
+const grammarTemplate = `Given a sentence correct the grammar.
+    sentence: {punctuated_sentence}
+    sentence with correct grammar: 
+    `;
+const grammarPrompt = PromptTemplate.fromTemplate(grammarTemplate);
+
+const chain = RunnableSequence.from([
+  punctuationPrompt,
+  llm,
+  new StringOutputParser(),
+  { punctuated_sentence: prevResult => prevResult },
+  grammarPrompt,
+  llm,
+  new StringOutputParser(),
+]);
+
+const response = await chain.invoke({
+  sentence: "i dont liked mondays",
+  language: "korean",
+});
+
+console.log(response);
