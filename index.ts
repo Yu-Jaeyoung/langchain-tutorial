862f23301e48d79c8e80911c78e936914f9659e5
Yu-Jaeyoung <jackyu0721@gmail.com>
2025-11-25 11:27:58 +0900

    chunking example

--- index.ts
+++ index.ts
@@ -1,1 +1,26 @@
-console.log("Hello via Bun!");
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+
+try {
+  const result = Bun.file("jaeyoung_info.txt");
+  const text = await result.text();
+
+  const splitter = new RecursiveCharacterTextSplitter();
+
+  const output = await splitter.createDocuments([ text ]);
+  console.info(output);
+
+  const document = { ...output[0] };
+  console.info(document.metadata);
+
+  //{
+  //   loc: {
+  //     lines: {
+  //       from: 1,
+  //       to: 9,
+  //     },
+  //   },
+  // }
+
+} catch (err) {
+  console.info(err);
+}
